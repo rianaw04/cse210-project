78de6f7bf1d023cfef41f2d65660a5f589664a2e
rianaw04 <warnerriana1@gmail.com>
2024-01-13 16:44:30 -0400

    Prep 2

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,11 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("Please enter your first name:  ");
-       string firstName = Console.ReadLine();
-        Console.Write("Please enter your last name: ");
-        string lastName = Console.ReadLine();
-
-        Console.WriteLine($"Your name is {lastName}, {firstName} {lastName}");
+      
     }
 }
--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,31 +4,6 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.Write("Please enter the secret number: ");
-        int secretNumber = int.Parse(Console.ReadLine());
-
-        Random randomGenerator = new Random();
-        int number = randomGenerator.Next(1,11);
-
-        int guess = -1;
-
-        while (guess != number)
-        {
-            Console.Write("Please enter you guess: ");
-            guess = int.Parse(Console.ReadLine());
-
-            if (number > guess)
-            {
-                Console.WriteLine("Higher");
-            }
-            else if (number < guess)
-            {
-                Console.WriteLine("Lower");
-            }
-            else
-            {
-                Console.WriteLine("You've guessed it!");
-            }
-        }
+       
     }
 }
