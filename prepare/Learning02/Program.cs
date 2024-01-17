6a0b560a6bf90acbc09f3e48142a99f6e92c57b6
rianaw04 <warnerriana1@gmail.com>
2024-01-17 15:42:09 -0400

    updated program.cs

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,23 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "Microsoft";
+        job1._jobTitle = "Software Engineer";
+        job1._startYear = 2010;
+        job1._endYear = 2015;
+
+
+        Job job2 = new Job();
+        job2._company = "Samsung";
+        job2._jobTitle = "Graphic Designer";
+        job2._startYear = 2016;
+        job2._endYear = 2023;
+
+        job1.Display();
+        job2.Display();
     }
+
+    //job1.Display();
+
 }
